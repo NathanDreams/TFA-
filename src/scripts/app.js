a28a9d9e50510612fa85343ba34530a6f3d8d47b
Nathan <nathan.collart.1604@gmail.com>
2023-06-04 17:30:22 +0200

    nouveau menu + debut placement image

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,17 @@
+window.onload = function () {
+	window.addEventListener('scroll', function (e) {
+		if (window.pageYOffset > 100) {
+			document.querySelector("header").classList.add('is-scrolling');
+		} else {
+			document.querySelector("header").classList.remove('is-scrolling');
+		}
+	});
 
-document.querySelector('.burger-menu').addEventListener('click', function() {
-    document.querySelector('.navbar').classList.toggle('active');
-  });
-  
+	const menu_btn = document.querySelector('.hamburger');
+	const mobile_menu = document.querySelector('.mobile-nav');
+
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('is-active');
+		mobile_menu.classList.toggle('is-active');
+	});
+}
